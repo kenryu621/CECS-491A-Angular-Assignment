fdaa9fd698826522d5217e515e3e6a273e3faa7d
Yifan Yu <kenryu422@outlook.com>
2023-04-13 10:32:28 -0700

    Add: Angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormTableComponent } from './form-table/form-table.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'form', component: ReactiveFormComponent },
+  { path: 'table', component: FormTableComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
